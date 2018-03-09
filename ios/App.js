31718e77948044c4626dc44f0424c68296587794
Andrew Eskenazi <drewesk@gmail.com>
2018-03-08 22:57:33 -0700

    Calc - First Commit!

---
+++ ios/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Welcome to the begginings of my Calculator!</Text>
+      </View>
+    );
+  }
+}
