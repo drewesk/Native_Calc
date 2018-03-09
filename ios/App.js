409c8242363b30177d732632aa95605617d3e8f0
Andrew Eskenazi <drewesk@gmail.com>
2018-03-09 15:13:38 -0700

    Building style and layout

--- ios/App.js
+++ ios/App.js
@@ -4,18 +4,39 @@ import { StyleSheet, Text, View } from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  top: {
+    paddingTop: 20,
+    backgroundColor: 'salmon',
+  },
+  bottom: {
+    flex: 1,
     backgroundColor: 'red',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  number: {
+    color: '#fff',
+    backgroundColor: 'slategrey',
+    textAlign: 'right',  
+    padding: 10,
+    fontSize: 30,
+    fontWeight: 'bold',
+    borderBottomWidth: 1,
+    borderColor: '#fff',
+  },
+  row: {
+
   },
 });
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Welcome to the begginings of my Calculator!</Text>
-      </View>
-    );
-  }
-}
+const App = () => <View style={ styles.container }>
+  <View style={ styles.top }>
+    <Text style={ styles.number } >0</Text>
+    <Text style={ styles.number } >0</Text>
+    <Text style={ styles.number } >0</Text>
+  </View>
+  <View style={ styles.bottom }>
+    <View style={ styles.row }>
+
+    </View>
+  </View>
+</View>
